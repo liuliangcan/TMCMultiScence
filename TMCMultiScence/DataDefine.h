682fc9f9a6df4efd5c2776480d92bd4d69464e73
LIU <LIU@LIU-PC>
2015-12-10 16:23:40 +0800

    修改了头文件xdrid为char[]

--- TMCMultiScence/DataDefine.h
+++ TMCMultiScence/DataDefine.h
@@ -1,22 +1,15 @@
 //
 //  DataDefine.h
+//  Prepare_Data
 //
 //  Created by 梁田 on 15/11/13.
 //  Copyright © 2015年 梁田. All rights reserved.
 //
 
-/*
- 各个接口结构按照移动集团信令采集解析规范V2.0.9定制
- 每个接口被注释掉的字段多场景识别暂时不使用
-*/
-
 #ifndef data_define_h
 #define data_define_h
 
-struct st_XDRID{
-    unsigned long long high;
-    unsigned long long low;
-};
+
 
 union IPV6{
     unsigned char addr[16];
@@ -43,11 +36,47 @@ union SGW_ADDR {
     IPV6        ipv6;
 };
 
+enum CoveringProperties
+{
+    USER_INDOOR = 0,     //室内用户
+    USER_OUTDOOR = 1     //室外用户
+};
+
+//小区覆盖子属性
+enum CoveringChildProperties
+{
+    INDOOR_HOME = 0,            //占用室分的室内稳定用户（有室分）
+    INDOOR_OUT = 1,             //占用宏站的室内稳定用户（无室分）
+    INDOOR_HOME_OUT = 2,        //室内稳定用户（占用宏站和室分）
+    OUTDOOR_SLOW = 3,           //室外低速用户（速度在20公里/小时）
+    OUTDOOR_MID = 4,            //室外中速用户（20~60公里/小时）
+    OUTDOOR_QUICK = 5           //室外高速用户（20~60公里/小时）
+};
+
+//小区场景属性
+enum ScenceProperties
+{
+    UNKNOWN_ECI = 0,            //未知类型
+    SCHOOL_ECI = 1,             //校园小区
+    LIVING_ECI = 2,             //居民楼小区
+    BUSSINESS_ECI = 3,          //商业区
+    SCENIC_ECI = 4,             //风景区
+    SUBWAY_ECI = 5,             //地铁
+    HIGHSPEED_ECI = 6           //高铁
+};
+
+//小区位置属性
+enum CellSiteProperties {
+    CELL_INDOOR  = 0,                //室内小区
+    CELL_OUTDOOR = 1,                //室外小区
+    
+} ;
+
 struct st_PUBLICPART {
-//    unsigned short          nLength;
+    unsigned short          nLength;
     unsigned short          nCity;
     unsigned char           cInterface;
-    st_XDRID                XdrId;
+    char                    XdrId[33];
     unsigned char           cRat;
     unsigned long long      llImsi;
     unsigned long long      llImei;
@@ -62,52 +91,63 @@ struct st_MME_PART{
     unsigned short          nRequestCause;
     unsigned short          nFailureCause;
     unsigned char           cKeyword_1;
-//    unsigned char           cKeyword_2;
-//    unsigned char           cKeyword_3;
-//    unsigned char           cKeyword_4;
-//    unsigned int            nMmeUES1apId;
-//    unsigned short          nOldMmeGroupId;
-//    unsigned char           cOldMmeCode;
-//    unsigned int            nOldMtmsi;
-//    unsigned short          nMmeGroupId;
-//    unsigned char           cMmeCode;
-//    unsigned int            nMtmsi;
-//    unsigned int            nTmsi;
-//    IPV4                    eIpv4;
-//    IPV6                    eIpv6;
-//    MME_ADDR                eMMEAddr;
-//    ENB_ADDR                eENBAddr;
-//    unsigned short          nMMEPort;
-//    unsigned short          nENBPort;
+    unsigned char           cKeyword_2;
+    unsigned char           cKeyword_3;
+    unsigned char           cKeyword_4;
+    unsigned int            nMmeUES1apId;
+    unsigned short          nOldMmeGroupId;
+    unsigned char           cOldMmeCode;
+    unsigned int            nOldMtmsi;
+    unsigned short          nMmeGroupId;
+    unsigned char           cMmeCode;
+    unsigned int            nMtmsi;
+    unsigned int            nTmsi;
+    IPV4                    eIpv4;
+    IPV6                    eIpv6;
+    MME_ADDR                eMMEAddr;
+    ENB_ADDR                eENBAddr;
+    unsigned short          nMMEPort;
+    unsigned short          nENBPort;
     unsigned short          nMMETAC;
     unsigned int            nMMECellID;
     unsigned short          nOtherTAC;
     unsigned int            nOtherCellid;
-//    unsigned char           sMMEAPN[32];
-  
-//承载暂不使用，数量为多个
-//    unsigned char           cEPSBearerNum;
-//    unsigned char           cBearer1ID;
-//    unsigned char           cBearer1Type;
-//    unsigned char           cBearer1QCI;
-//    unsigned char           cBearer1Status;
-//    unsigned int            nBearer1ENBTeid;
-//    unsigned int            nBearer1SGWTeid;
-   
+    unsigned char           sMMEAPN[32];
+    
+    unsigned char           cEPSBearerNum;
+    unsigned char           cBearer1ID;
+    unsigned char           cBearer1Type;
+    unsigned char           cBearer1QCI;
+    unsigned char           cBearer1Status;
+    unsigned int            nBearer1ENBTeid;
+    unsigned int            nBearer1SGWTeid;
+    unsigned char           cBearer2ID;
+    unsigned char           cBearer2Type;
+    unsigned char           cBearer2QCI;
+    unsigned char           cBearer2Status;
+    unsigned int            nBearer2ENBTeid;
+    unsigned int            nBearer2SGWTeid;
+    unsigned char           cBearer3ID;
+    unsigned char           cBearer3Type;
+    unsigned char           cBearer3QCI;
+    unsigned char           cBearer3Status;
+    unsigned int            nBearer3ENBTeid;
+    unsigned int            nBearer3SGWTeid;
+    
 };
 
 
 struct st_HTTP_PART {
-//    unsigned char           cMachineIDAddType;
-//    SGW_ADDR                eSgwAddr;
-//    ENB_ADDR                eEnbAddr;
-//    unsigned short          nSgwPort;
-//    unsigned short          nEnbPort;
-//    unsigned int            nEnbTeid;
-//    unsigned int            nSgwTeid;
+    unsigned char           cMachineIDAddType;
+    SGW_ADDR                eSgwAddr;
+    ENB_ADDR                eEnbAddr;
+    unsigned short          nSgwPort;
+    unsigned short          nEnbPort;
+    unsigned int            nEnbTeid;
+    unsigned int            nSgwTeid;
     unsigned short          nHTTPTAC;
     unsigned int            nHTTPCellID;
-//    unsigned char           sHTTPAPN[32];
+    unsigned char           sHTTPAPN[32];
     unsigned char           cAppTypeCode;
     unsigned long long      llHTTPStartTime;
     unsigned long long      llHTTPEndTime;
@@ -116,13 +156,13 @@ struct st_HTTP_PART {
     unsigned short          nAppSubType;
     unsigned char           cAppContent;
     unsigned char           cAppStatus;
-//    IPV4                    eUserIPv4;
-//    IPV6                    eUserIPv6;
-//    unsigned short          nUserPort;
-//    unsigned char           cL4Protocal;
-//    IPV4                    eServerIPv4;
-//    IPV6                    eServerIPv6;
-//    unsigned short          nAppServerPort;
+    IPV4                    eUserIPv4;
+    IPV6                    eUserIPv6;
+    unsigned short          nUserPort;
+    unsigned char           cL4Protocal;
+    IPV4                    eServerIPv4;
+    IPV6                    eServerIPv6;
+    unsigned short          nAppServerPort;
     unsigned int            nULFlowByte;
     unsigned int            nDLFlowByte;
     unsigned int            nULIPPackets;
@@ -137,8 +177,8 @@ struct st_HTTP_PART {
     unsigned int            nDLIPFragPackets;
     unsigned int            nFirReqHandAckDelay;
     unsigned int            nFirResReqDelay;
-//    unsigned int            nWindowSize;
-//    unsigned int            nMSS;
+    unsigned int            nWindowSize;
+    unsigned int            nMSS;
     unsigned char           cTcpSetupTrys;
     unsigned char           cTcpLinkStatus;
     unsigned char           cSessionOver;
@@ -148,7 +188,7 @@ struct st_HTTP_PART {
     unsigned int            nFirstResDelay;
     unsigned int            nLastResDelay;
     unsigned int            nLastACKDelay;
-
+    /*字段太长，暂时不使用*/
 //    unsigned char           sHOST[64];
 //    unsigned char           sURL[512];
 //    unsigned char           sXOnlineHost[128];
@@ -162,12 +202,11 @@ struct st_HTTP_PART {
 //    unsigned char           cWtpFailReason;
 //    unsigned char           sTitle[256];
 //    unsigned char           sKeyword[256];
-    
-//    unsigned char           cServiceAction;
-//    unsigned char           cSuccess;
-//    unsigned int            nServiceDelay;
-//    unsigned char           cBrowseTool;
-//    unsigned char           cWebsite;
+    unsigned char           cServiceAction;
+    unsigned char           cSuccess;
+    unsigned int            nServiceDelay;
+    unsigned char           cBrowseTool;
+    unsigned char           cWebsite;
     
     //切分http之后新加入的字段
     unsigned long long      llStartTimeNew;
@@ -182,7 +221,6 @@ struct st_HTTP_PART {
 
 };
 
-//硬采额外附件字段
 struct st_ADDITIONAL_PART  {
     unsigned char           cVendorId;              //供应商标识 1-中兴 2-华为 3-爱立信
     unsigned long long      nStartTime;             //xdr中的startTime
@@ -207,9 +245,10 @@ struct st_ADDITIONAL_PART  {
     unsigned char           cCoveringProperties;            //覆盖属性
     unsigned char           cCoveringChildProperties;       //覆盖子属性
     unsigned char           cScenceProperties;              //场景属性
-    float                   nSpeed;                         //速度
+    unsigned int            nSpeed;                         //速度
     unsigned char           cCellScenceProperties;          //服务小区场景属性
     unsigned char           cCellIndoorProperties;          //服务小区室分宏站属性
+    
     
 };
 
@@ -218,88 +257,31 @@ struct st_UU_PART {
     unsigned long long      llUUStartTime;
     unsigned long long      llUUEndTime;
     unsigned char           cKeyword1;
-//    unsigned char           cKeyword2;
+    unsigned char           cKeyword2;
     unsigned char           cProcedureStatus;
-//    unsigned char           sPLMNID[3];
+    unsigned char           sPLMNID[3];
     unsigned int            nEnbId;
     unsigned int            nCellid;
     unsigned short          nCRNTI;
     unsigned int            nTargetEnbid;
     unsigned int            nTargetCellid;
-//    unsigned short          nTargetnCRNTI;
-//    unsigned int            nMmeUES1apId;
-//    unsigned short          nMmeGroupId;
-//    unsigned char           cMmeCode;
-//    unsigned int            nMtmsi;
-    unsigned char           cCSFBIndication;
-    unsigned char           cRedirectedNetwork;
-  
-//    unsigned char           cEPSBearerNum;
-//    unsigned char           cBearer1ID;
-//    unsigned char           cBearer1Type;
-//    unsigned char           cBearer1QCI;
-//    unsigned char           cBearer1Status;
-//    unsigned int            nBearer1ENBTeid;
-//    unsigned int            nBearer1SGWTeid;
+    unsigned short          nTargetnCRNTI;
     
 };
 
 struct st_X2_PART {
-    unsigned char           cProcedureType;
-    unsigned long long      llX2StartTime;
-    unsigned long long      llX2EndTime;
-    unsigned char           cProcedureStatus;
-    unsigned int            nSourceCellid;
-    unsigned int            nSourceEnbid;
-    unsigned int            nTargetCellid;
-    unsigned int            nTargetEnbid;
-    unsigned int            nMmeUES1apId;
-    unsigned short          nMmeGroupId;
-    unsigned char           cMmeCode;
-    unsigned short          nRequestCause;
-    unsigned short          nFailureCause;
     
-//承载暂不使用，数量为多个
-//    unsigned char           cEPSBearerNum;
-//    unsigned char           cBearer1ID;
-//    unsigned char           cBearer1Type;
-//    unsigned char           cBearer1QCI;
-//    unsigned char           cBearer1Status;
-//    unsigned int            nBearer1ENBTeid;
-//    unsigned int            nBearer1SGWTeid;
-
 };
 
 struct st_UEMR_PART {
-//    unsigned short          nMmeGroupId;
-//    unsigned char           cMmeCode;
-//    unsigned int            nMmeUES1apId;
-    unsigned int            nEnbid;
-    unsigned int            nCellid;
-    unsigned long long      llTime;
-    unsigned char           cMRtype;
-    unsigned char           cPHR;
-    unsigned short          nEnbReceivedPower;
-    unsigned char           cUL_SINR;
-    unsigned short          nTA;
-    unsigned short          nAoA;
-    unsigned short          nServingFreq;
-    unsigned char           cServingRSRP;
-    unsigned char           cServingRSRQ;
     
-//邻区暂不使用，数量为多个
-//    unsigned char           cNeighborCellNumber;
-//    unsigned short          nNeighbor1_CellPCI;
-//    unsigned short          nNeighbor1_Freq;
-//    unsigned char           nNeighbor1_RSRP;
-//    unsigned char           nNeighbor1_RSRQ;
 };
 
-//软采额外附加字段
 struct st_RC_ADDITIONAL_PART {
     
     unsigned char           cVendorId;                  //供应商标识 1-中兴 2-华为 3-爱立信
     unsigned long long      nStartTime;                 //xdr中的startTime
+    
     double                  dLongitude;                 //计算后的经度
     double                  dLatitude;                  //计算后的维度
     unsigned char           sGridID[20];                //栅格ID
@@ -309,33 +291,34 @@ struct st_RC_ADDITIONAL_PART {
     
 };
 
-/*软采结构*/
 struct st_RC_XDR_INFO {
-    
-    st_PUBLICPART           public_part;                //XDR公共部分
-    
-    union PRIVATE_PART {
+    st_PUBLICPART           public_part;
+    union PRIVATE_PART{
         st_X2_PART          x2_part;
         st_UU_PART          uu_part;
         st_UEMR_PART        uemr_part;
-    } rc_private_part;
+    }rc_private_part;
     
-    st_RC_ADDITIONAL_PART      rc_additional_part;          //额外增加部分
+    st_RC_ADDITIONAL_PART      rc_additional_part;
     
 };
 
-/*硬采结构*/
+
+
+
 struct st_XDR_INFO {
     
     st_PUBLICPART           public_part;                    //XDR公共部分
     
-    union PRIVATE_PART {
+    union PRIVATE_PART{
         st_MME_PART         mme_part;
         st_HTTP_PART        http_part;
-    } private_part;
+    }private_part;
     
     st_ADDITIONAL_PART      additional_part;                //额外增加部分
     
 };
+
+
 
 #endif /* data_define_h */
