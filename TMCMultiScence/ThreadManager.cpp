e69e36f95ab034ba1b12cc40379c16b4ce39ccd5
LIU <LIU@LIU-PC>
2015-12-10 16:25:18 +0800

    优化了批次结束判断消息

--- TMCMultiScence/ThreadManager.cpp
+++ TMCMultiScence/ThreadManager.cpp
@@ -181,10 +181,10 @@ THREAD_RETURN THREAD_PROC FileHandleProc(THREAD_PARAM param)
             {
                 emptytime = time(0);
                 unsigned int d_time = emptytime - lastdealtime;
-                if(!hasWriteEndFile && d_time > GlobalConfiger::GetInstance()->GetUMultiScenceEndDelay() && bDealing == 0)
+                if(bDealing == 0 && !hasWriteEndFile && d_time > GlobalConfiger::GetInstance()->GetUMultiScenceEndDelay())
                 {
                     int batchover = (int)time(0);
-                    printf("这一批处理完了[%d]\n", batchover);
+                    printf("本批次处理结束[%d]\n", batchover);
                     LOG_NOTICE("[FileHandleProc]批次处理结束[%d]", batchover);
                     std::ofstream endfileout(GlobalConfiger::GetInstance()->GetUMultiScenceEndBatchFile().c_str());
                     if(!endfileout.is_open())
